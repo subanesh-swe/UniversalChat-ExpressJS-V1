226b6a7e649f48650b7b8c11ed2b9021aac0ce5d
Subanesh-swe <subaneshtechie@gmail.com>
2023-06-26 01:59:31 +0530

    created modern dorm , added checkbox

--- public/scripts/formHandler.js
+++ public/scripts/formHandler.js
@@ -16,15 +16,39 @@ function formSubmitHandler(event, formId) {
         .then(data => {
             console.log("response data = " + JSON.stringify(data));
             if (data.result === true) {
-                //window.location.href = data.redirect;
+                window.location.href = data.redirect;
             } else {
-                //alert(data.alert);
+                alert(data.alert);
             }
         })
         .catch(error => {
-            //alert("Something went wrong, Try again after sometime!!!");
+            alert("Something went wrong, Try again after sometime!!!");
             console.error(error);
         });
+}
+
+function checkInputLength(input) {
+    condole.log(`input...`);
+    if (input.value.length > input.maxLength) {
+        condole.log(`input... reached limit`);
+        input.setCustomValidity(`You cannot type more than ${input.maxLength} characters.`);
+    } else {
+        condole.log(`input... within limit`);
+        input.setCustomValidity("");
+    }
+}
+
+function togglePassword(eyeBtnId, passwordFieldId) {
+    const eyeBtn = document.getElementById(eyeBtnId);
+    const passwordField = document.getElementById(passwordFieldId);
+
+    if (passwordField.type === "password") {
+        eyeBtn.setAttribute("class", "eye-slash");
+        passwordField.type = "text";
+    } else {
+        eyeBtn.setAttribute("class", "eye");
+        passwordField.type = "password";
+    }
 }
 
 //function formSubmitHandler(event) {
