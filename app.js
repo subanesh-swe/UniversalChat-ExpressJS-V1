12ef7e9f507654e61f6b69d40352267907583999
subanesh-swe <subaneshtechie@gmail.com>
2023-09-02 09:38:27 +0530

    modified app.js

--- app.js
+++ app.js
@@ -67,31 +67,30 @@ app.use(session({
         'cookieValue': "Universal chat by Subanesh",
         maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
         httpOnly: true,
-        secure: true,
     },
     store: store,
     resave: true,
     saveUninitialized: true
 }));
 
-app.use('/users/login', (req, res, next) => {
-    const detector = new DeviceDetector({
-        clientIndexes: true,
-        deviceIndexes: true,
-        deviceAliasCode: false,
-    });
+//app.use('/users/login', (req, res, next) => {
+//    const detector = new DeviceDetector({
+//        clientIndexes: true,
+//        deviceIndexes: true,
+//        deviceAliasCode: false,
+//    });
 
-    //const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
-    //const result = detector.detect(userAgent);
+//    //const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; NX505J Build/KVT49L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.78 Mobile Safari/537.36';
+//    //const result = detector.detect(userAgent);
 
-    const userAgent = req.headers['user-agent'];
-    const result = detector.detect(userAgent);
+//    const userAgent = req.headers['user-agent'];
+//    const result = detector.detect(userAgent);
 
-    req.session.deviceDetails = result;
-    req.session.updateAt = Date.now();
-    console.log("in update of sessions-----------------------------------");
-    next();
-});
+//    req.session.deviceDetails = result;
+//    req.session.updateAt = Date.now();
+//    console.log("in update of sessions-----------------------------------");
+//    next();
+//});
 
 /*app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
@@ -156,18 +155,29 @@ io.on('connection', socket => {
     })
 })
 
-// Make io accessible to our router
-app.use(function (req, res, next) {
-    req.io = io;
-    next();
-});
+//// Make io accessible to our router
+//app.use(function (req, res, next) {
+//    req.io = io;
+//    next();
+//});
 
 /*
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 */
 
-app.use('/', indexRouter);
+app.all('/', function (req, res, next) {
+    //res.redirect('/index/rooms');
+    if (req.method === 'GET') {
+        console.log(`@/ > Main Router [ router.use GET ] :  req[path]: ${req.path}, redir >>> @/users/login`);
+        res.redirect('/users/login');
+    } else {
+        console.log(`@/ > Main Router [ router.use ALL ] :  req[path]: ${req.path}, sending(report)`);
+        return res.json({ result: true, redirect: "/users/login", alert: "Access Denied!!! either due to Illegal access to server or communication error, please Login!!!" });
+    }
+})
+
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 
@@ -186,3 +196,229 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
+
+///* Original File */
+////--------------------------./bin/www---------------------------
+//#!/usr/bin/env node
+
+///**
+// * Module dependencies.
+// */
+
+//var app = require('../app');
+//var debug = require('debug')('universalchat-nodejs:server');
+//var http = require('http');
+
+///**
+// * Get port from environment and store in Express.
+// */
+
+//var port = normalizePort(process.env.PORT || '3000');
+//app.set('port', port);
+
+///**
+// * Create HTTP server.
+// */
+
+//var server = http.createServer(app);
+
+///**
+// * Listen on provided port, on all network interfaces.
+// */
+
+//server.listen(port);
+//server.on('error', onError);
+//server.on('listening', onListening);
+
+///**
+// * Normalize a port into a number, string, or false.
+// */
+
+//function normalizePort(val) {
+//    var port = parseInt(val, 10);
+
+//    if (isNaN(port)) {
+//        // named pipe
+//        return val;
+//    }
+
+//    if (port >= 0) {
+//        // port number
+//        return port;
+//    }
+
+//    return false;
+//}
+
+///**
+// * Event listener for HTTP server "error" event.
+// */
+
+//function onError(error) {
+//    if (error.syscall !== 'listen') {
+//        throw error;
+//    }
+
+//    var bind = typeof port === 'string'
+//        ? 'Pipe ' + port
+//        : 'Port ' + port;
+
+//    // handle specific listen errors with friendly messages
+//    switch (error.code) {
+//        case 'EACCES':
+//            console.error(bind + ' requires elevated privileges');
+//            process.exit(1);
+//            break;
+//        case 'EADDRINUSE':
+//            console.error(bind + ' is already in use');
+//            process.exit(1);
+//            break;
+//        default:
+//            throw error;
+//    }
+//}
+
+///**
+// * Event listener for HTTP server "listening" event.
+// */
+
+//function onListening() {
+//    var addr = server.address();
+//    var bind = typeof addr === 'string'
+//        ? 'pipe ' + addr
+//        : 'port ' + addr.port;
+//    debug('Listening on ' + bind);
+//}
+
+////--------------------------app.js---------------------------
+//var createError = require('http-errors');
+//var express = require('express');
+//var path = require('path');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+//var app = express();
+
+//// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'pug');
+
+//app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//  next(createError(404));
+//});
+
+//// error handler
+//app.use(function(err, req, res, next) {
+//  // set locals, only providing error in development
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//  // render the error page
+//  res.status(err.status || 500);
+//  res.render('error');
+//});
+
+//module.exports = app;
+////--------------------------./routes/index.js---------------------------
+//var express = require('express');
+//var router = express.Router();
+
+///* GET home page. */
+//router.get('/', function (req, res, next) {
+//    res.render('index', { title: 'Express' });
+//});
+
+//module.exports = router;
+
+////--------------------------./routes/users.js---------------------------
+//var express = require('express');
+//var router = express.Router();
+
+///* GET users listing. */
+//router.get('/', function (req, res, next) {
+//    res.send('respond with a resource');
+//});
+
+//module.exports = router;
+
+////--------------------------./public/stylesheets/style.css---------------------------
+//body {
+//    padding: 50px;
+//    font: 14px "Lucida Grande", Helvetica, Arial, sans - serif;
+//}
+
+//a {
+//    color: #00B7FF;
+//}
+
+////--------------------------./views/index.pug---------------------------
+//extends layout
+
+//block content
+//h1 = title
+//  p Welcome to #{ title }
+
+////--------------------------./views/layout.pug---------------------------
+//doctype html
+//html
+//head
+//title = title
+//link(rel = 'stylesheet', href = '/stylesheets/style.css')
+//body
+//    block content
+
+////--------------------------./views/error.pug---------------------------
+//extends layout
+
+//block content
+//h1 = message
+//h2 = error.status
+//  pre #{ error.stack }
+
+////--------------------------./nuget.config---------------------------
+//<?xml version="1.0" encoding="utf-8"?>
+//<configuration>
+//  <packageSources>
+//    <clear />
+//    <add key="nuget.org" value="https://api.nuget.org/v3/index.json" />
+//  </packageSources>
+//  <disabledPackageSources>
+//    <clear />
+//  </disabledPackageSources>
+//</configuration>
+
+////--------------------------./package.json---------------------------
+//{
+//  "name": "universalchat-nodejs",
+//  "version": "0.0.0",
+//  "private": true,
+//  "scripts": {
+//    "start": "node ./bin/www"
+//  },
+//  "dependencies": {
+//    "cookie-parser": "~1.4.4",
+//    "debug": "~2.6.9",
+//    "express": "~4.16.1",
+//    "http-errors": "~1.6.3",
+//    "morgan": "~1.9.1",
+//    "pug": "2.0.0-beta11"
+//  }
+//}
+
+/* Original Files */
